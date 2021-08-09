b74925d78f0464cab5392dcab0791b5897c2f6b5
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-09 10:20:44 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
-# Export environ variables 
-# export NEWS_API_KEY='70a9b9d3f1624d63b54bf7ddd06b8c4d'
-
 export NEWS_API_KEY='b4b8f35f4a5444c2b8b14554a27d1676'
-python3 manage.py server
+python3.9 manage.py server
